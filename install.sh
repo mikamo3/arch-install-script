1d7165d38e06b392bb9a2f93ef00a96a9b1f5282
mikamo3 <kamo3proj@gmail.com>
2020-11-01 02:40:19 +0900

    add install script

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ ENABLE_MAKE_BOOT_PARTITION=""
 ROOT_PARTITION_LABEL_NAME="arch"
 BOOT_PARTITION_SIZE="512M"
 ROOT_PARTITION_SIZE=
+TIMEZONE="Asia/Tokyo"
+LOCALES=("en_US.UTF-8 UTF-8" "ja_JP.UTF-8 UTF-8")
+LOCALE_LANG="ja_JP.UTF-8 UTF-8"
+UCODE="intel"
 create_partition() {
   echo "Partition Layout"
   read -n1 -r -p "do you want to create boot partition? (y/N): " ENABLE_MAKE_BOOT_PARTITION
@@ -82,7 +86,7 @@ mount_partition() {
 install_base_package() {
   echo "install base package"
   timedatectl set-ntp true
-  pacstrap /mnt base ansible git
+  pacstrap /mnt base linux linux-firmware openssh ${UCODE}-ucode btrfs-progs networkmanager sudo
 }
 
 create_fstab() {
@@ -90,10 +94,62 @@ create_fstab() {
   genfstab -U /mnt >/mnt/etc/fstab
 }
 
+set_root_password() {
+  arch-chroot /mnt passwd
+}
+
+set_timezone() {
+  arch-chroot /mnt ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+  arch-chroot /mnt hwclock --systohc
+}
+
+set_locale() {
+  echo "" >/mnt/etc/locale.gen
+  for locale in "${LOCALES[@]}"; do
+    echo "$locale" >>/mnt/etc/locale.gen
+  done
+  arch-chroot /mnt locale-gen
+  echo "LANG=$LOCALE_LANG" >/etc/locale.conf
+}
+
+create_users() {
+  # for exec ansible
+  arch-chroot /mnt useradd -s /sbin/nologin -u 2000 ansible
+  arch-chroot /mnt passwd ansible
+  echo "ansible   ALL=(ALL) ALL" >> /mnt/etc/sudoers
+}
+
+set_bootloader() {
+  arch-chroot /mnt mkinitcpio -P
+  arch-chroot /mnt bootctl install
+  cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /${UCODE}-ucode.img
+initrd  /initramfs-linux.img
+options root=PARTLABEL=arch rootflags=subvol=@ rw
+EOF
+  cat <<EOF >/mnt/boot/loader/loader.conf
+default arch
+timeout 4
+editor no
+EOF
+}
+
+enable_service() {
+  arch-chroot /mnt systemctl enable NetworkManager
+  arch-chroot /mnt systemctl enable sshd
+}
 create_partition
 make_subvolumes
 mount_partition
 install_base_package
+set_root_password
 create_fstab
+set_timezone
+set_locale
+create_users
+set_bootloader
+enable_service
 
 echo "DONE"
